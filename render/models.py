c3cb72ce235947da154bc27a66763512a4fca903
Your Name <you@example.com>
2022-05-12 19:19:17 +0530

    add

--- render/models.py
+++ render/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Items(models.Model):
+    item_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.item_name
